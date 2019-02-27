a957c71614c66320b557d3caa1f1b3c80a9a2f60
Jesús Rojas <jrojas@tecnopress.pe>
2019-02-27 10:30:01 -0500

    Armando la estructura del proyecto

---
+++ GHMKP/Model/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHMKP.Model
+{
+    public class Cliente
+    {
+        public string CodCliente { get; set; }
+        public string NomCliente { get; set; }
+        public string DirCliente { get; set; }
+        public string TelCliente { get; set; }
+        public decimal DirLong { get; set; }
+        public decimal DirLat { get; set; }
+    }
+}
